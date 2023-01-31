52ef353314203dae6511c00e734dac8d17e47fe4
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-02-01 00:08:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const router = require('./router');
+io.on('connection', (socket) => {
+    console.log('We have a new connection!!!');
+
+    socket.on('disconnect', () => {
+        console.log('User had left!!!');
+    });
+});
 
 app.use(router);
 
