aec8e2ca128d9e3f8f0571653e7835a3c6c55261
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-02-01 00:01:57 +0100

    run server

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
-const router = require('./router');
-
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app().use(router);
+const router = require('./router');
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Server is up and running');
+    res.send('Server is up and running', 200);
 });
 
 module.exports = router;
