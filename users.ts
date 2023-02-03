d8039eb810d3f29216b7553abd48444a23ae3a4f
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-02-03 22:52:08 +0100

    add user funct.

--- users.ts
+++ users.ts
@@ -1,16 +1,34 @@
 const users = [];
 
-const addUser = ({ id, name, room }) => {
+const addUser = ({id, name, room}) => {
     name = formatName(name);
     room = formatName(room);
+
+    const userAlreadyExists: boolean = users.find((user) => {
+        user.name === name && user.room === room
+    });
+
+    if (userAlreadyExists) {
+        return {error: 'Username is taken!!!'};
+    }
+
+    const user = { id, name, room };
+
+    users.push(user);
+
+    return user;
+
 };
 
-const removeUser = () => {};
+const removeUser = () => {
+};
 
-const getUser = () => {};
+const getUser = () => {
+};
 
-const getUsersOfRoom = () => {};
+const getUsersOfRoom = () => {
+};
 
-var formatName = (name : string) : string => {
+var formatName = (name: string): string => {
     return name.trim().toLocaleLowerCase().replace(' ', '');
 }
