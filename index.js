0641c1aaf2e87c675b804a884498ce8e61a11fcb
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-02-03 14:37:33 +0100

    add CORS options

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,6 +10,11 @@ const router = require('./router');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+const corsOptions = {
+    origin: 'http://localhost:3000/',
+    optionsSuccessStatus: 200
+};
 
 io.on('connection', (socket) => {
     console.log('We have a new connection!!!');
@@ -19,5 +25,6 @@ io.on('connection', (socket) => {
 });
 
 app.use(router);
+app.use(cors(corsOptions));
 
 server.listen(PORT, () => console.log(`Server has started on localhost:${PORT}`));
